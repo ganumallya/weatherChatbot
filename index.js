f5eb5d8b35401675fcee437426971fed221c3db5
Ganesh Mallya <ganumallya@yahoo.co.in>
2016-12-16 00:15:05 +0530

    made port changes to index.js

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/webhook', function (req, res) {
-    res.write('<h1>Hello</h1>')
+    res.write('<h1>Hello</h1>');
+});
+
+app.get('/',function(req,res){
+	res.write('<h1>Home Page</h1>');
 })
 
 
-app.listen(3000,function(){
+app.listen(app.get('port'),function(){
 	console.log('Example app listening on port 3000')
 })
