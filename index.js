a7940cbada2dfcde2305c80bb5c7658c198e8fe1
Ganesh Mallya <ganumallya@yahoo.co.in>
2016-12-16 13:24:27 +0530

    Added post request to /webhook route

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ var server = require('http').createServer(app);
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/webhook', function (req, res) {
-    res.write('<h1>Hello</h1>');
+app.get('/webhook',function(req,res){
+	res.write('<h1> In Webhook </h1>');
+});
+
+app.post('/webhook', function (req, res) {
+
+    finalResponse = {"fulfillment": {
+  "speech": "Today in Boston: Fair, the temperature is 37 F",
+  "source": "apiai-weather-webhook-sample",
+  "displayText": "Today in Boston: Fair, the temperature is 37 F"
+}};
+		res.status(200)
+		res.send(finalResponse);
 });
 
 app.get('/',function(req,res){
@@ -14,5 +25,5 @@ app.get('/',function(req,res){
 
 
 app.listen(app.get('port'),function(){
-	console.log('Example app listening on port 3000')
+	console.log('Example app listening on port 5000')
 })
