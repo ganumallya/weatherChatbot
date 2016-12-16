6c96ee1095e4a33b9f4f341433d0fea82a36af24
Ganesh Mallya <ganumallya@yahoo.co.in>
2016-12-17 00:18:30 +0530

    added custom and random messages as response

--- index.js
+++ index.js
@@ -25,12 +25,42 @@ app.post('/webhook', function (req, res) {
   	getWeather(city,function(t,msg){
   		
   		if(t){
+  			var finalSpeech = '';
   			var finalTemperature = parseInt(msg.main["temp"]) - 273.15; 
   			finalTemperature = Math.round(finalTemperature);
   			res.status(200);
+  			var pWeather = ["Weather is very pleasant right now","The weather is too good at the moment","Its neither too hot nor too cold"];
+  			var cWeather = ["Its very chilling out here","Damn its cold","I will be freezed if i was there","its super cold out there"];
+  			var hWeather = ["Hot like frying pan","i am gonna melt away at this temperature, its just too hot","Damn its hot"];
+  			var rWeather = ["Weather is cloudy","lot of clouds in the sky","Its gonna rain today"];
+  			if (finalTemperature>30){
+  				finalSpeech += hWeather[Math.floor(Math.random()*(hWeather.length))] + " its " + finalTemperature + " Degree right now";
+  			}else if(finalTemperature>15){
+  				finalSpeech += pWeather[Math.floor(Math.random()*(pWeather.length))] + " its " + finalTemperature + " Degree right now";
+  			}else{
+  				finalSpeech += cWeather[Math.floor(Math.random()*(cWeather.length))] + " its " + finalTemperature + " Degree right now";
+  			};
+
+  			var tempID = parseInt(msg["weather"][0].id);
+  			if(tempID<300){
+  				finalSpeech += ", and its raining heavily too";
+  			}else if(tempID<400){
+  				finalSpeech += ", and its drizling";
+  			}else if(tempID<600){
+  				finalSpeech += ", and its raining now";
+  			}else if(tempID<700){
+  				finalSpeech += ", and its snowing too";
+  			}else if(tempID<800){
+  				finalSpeech += " ";
+  			}else if(tempID==800){
+  				finalSpeech += " ,and sky looks clear";
+  			}else if(tempID <900){
+  				finalSpeech += ", and there is clouds scaterred over the sky";
+  			}
+
   		 finalResponse = {
-  						"speech": "Temperature right now is "+finalTemperature,
-  						"displayText": "Temperature right now is "+finalTemperature
+  						"speech": finalSpeech,
+  						"displayText": finalSpeech
   						};
 		res.send(finalResponse);
   		}
